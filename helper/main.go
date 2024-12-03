b2702d83754e16a23448448cab792c3f90cb4e46
Sergey Chubaryan <serghio2@yandex.ru>
2024-12-04 01:22:52 +0300

    added config for helper service

--- helper/main.go
+++ helper/main.go
@@ -5,58 +5,73 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/segmentio/kafka-go"
 	"gopkg.in/gomail.v2"
+	"gopkg.in/yaml.v3"
 )
 
-// type emailHelper struct {
-// 	dialer *gomail.Dialer
-// }
+const MSG_TEXT = `
+<html>
+	<head>
+	</head>
+	<body>
+		<p>This message was sent because you forgot a password</p>
+		<p>To change a password, use <a href="{{Link}}"/>this</a> link</p>
+	</body>
+</html>
+`
 
-// func (e *emailHelper) SendEmailForgotPassword(email, token string) {
-// 	link := "https://nucrea.ru?token=" + token
+func SendEmailForgotPassword(dialer *gomail.Dialer, from, to, token string) error {
+	link := "localhost:8080/restore-password?token=" + token
 
-// 	const MSG_TEXT = `
-// 	<html>
-// 		<head>
-// 		</head>
-// 		<body>
-// 			<p>This message was sent because you forgot a password</p>
-// 			<p>To change a password, use <a href="{{Link}}"/>this</a> link</p>
-// 		</body>
-// 	</html>
-// 	`
-// 	msgText := strings.ReplaceAll(MSG_TEXT, "{{Link}}", link)
+	msgText := strings.ReplaceAll(MSG_TEXT, "{{Link}}", link)
 
-// 	m := gomail.NewMessage()
-// 	m.SetHeader("From", "email")
-// 	m.SetHeader("To", email)
-// 	m.SetHeader("Subject", "Hello!")
-// 	m.SetBody("text/html", msgText)
+	m := gomail.NewMessage()
+	m.SetHeader("From", m.FormatAddress(from, "Pet Backend"))
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", "Hello!")
+	m.SetBody("text/html", msgText)
 
-// 	if err := d.DialAndSend(m); err != nil {
-// 		panic(err)
-// 	}
-// }
+	return dialer.DialAndSend(m)
+}
+
+type Config struct {
+	Kafka struct {
+		Brokers []string `yaml:"brokers"`
+		Topic   string   `yaml:"topic"`
+	}
+
+	SMTP struct {
+		Server   string `yaml:"server"`
+		Port     int    `yaml:"port"`
+		Email    string `yaml:"email"`
+		Password string `yaml:"password"`
+	} `yaml:"smtp"`
+}
 
 func main() {
-	const (
-		SMTP_SERVER   = "smtp.yandex.ru"
-		SMTP_PORT     = 587
-		SMTP_LOGIN    = "serghio2@yandex.ru"
-		SMTP_PASSWORD = "ercutguhcfuzbvyl"
-	)
-
 	ctx := context.Background()
 
-	d := gomail.NewDialer(SMTP_SERVER, SMTP_PORT, SMTP_LOGIN, SMTP_PASSWORD)
+	configFile, err := os.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	config := &Config{}
+	if err := yaml.Unmarshal(configFile, config); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	dialer := gomail.NewDialer(config.SMTP.Server, config.SMTP.Port, config.SMTP.Email, config.SMTP.Password)
 
 	log.Println("starting reader...")
 
 	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{"localhost:9092"},
-		Topic:   "backend_events",
+		Brokers: config.Kafka.Brokers,
+		Topic:   config.Kafka.Topic,
 		GroupID: "consumer-group-id",
 	})
 
@@ -90,13 +105,7 @@ func main() {
 			continue
 		}
 
-		m := gomail.NewMessage()
-		m.SetHeader("From", m.FormatAddress("serghio2@yandex.ru", "Pet Backend"))
-		m.SetHeader("To", value.Email)
-		m.SetHeader("Subject", "Hello!")
-		m.SetBody("text/html", "Test backend")
-
-		if err := d.DialAndSend(m); err != nil {
+		if err := SendEmailForgotPassword(dialer, config.SMTP.Email, value.Email, value.Token); err != nil {
 			log.Fatalf("failed to send email: %s\n", err.Error())
 			continue
 		}
