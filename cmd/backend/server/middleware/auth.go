b4c1004cf2616468b7d45b2b4cb067e95d60d1f4
Sergey Chubaryan <serghio2@yandex.ru>
2025-02-17 11:24:37 +0300

    add restore password endpoints

--- cmd/backend/server/middleware/auth.go
+++ cmd/backend/server/middleware/auth.go
@@ -1,11 +1,19 @@
 package middleware
 
 import (
+	"backend/internal/core/models"
 	"backend/internal/core/services"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
+
+func GetUserFromRequest(c *gin.Context) *models.UserDTO {
+	if user, ok := c.Get("user"); ok {
+		return user.(*models.UserDTO)
+	}
+	return nil
+}
 
 func NewAuthMiddleware(userService services.UserService) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
